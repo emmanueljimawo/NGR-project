6bb3dc87e2b0f52b7fce37b80e34652e39185b97
Emmanuel Jimawo <emmanueljimawo@Emmanuels-MacBook-Pro.local>
2021-11-26 17:43:39 +0100

    update

--- ngr_website/core/static/js/main.js
+++ ngr_website/core/static/js/main.js
@@ -11,3 +11,64 @@ function scrollToTop() {
   window.scrollTo({top: 0, behavior: 'smooth'});
   
   }
+
+
+  function socialWindow(url) {
+    var left = (screen.width - 570) / 2;
+    var top = (screen.height - 570) / 2;
+    var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
+    window.open(url, "NewWindow", params);
+  }
+  
+    var tweet = encodeURIComponent($("meta[property='og:description']").attr("content"));
+    
+    $(".twitter").click(function(e) {
+      e.preventDefault();
+  
+      var this_ = $(this)
+      var path = this_.attr("data-url");
+      var pageUrl = `${origin}${path}`
+      url = `https://twitter.com/intent/tweet?url=${pageUrl}&text=${tweet}?utm_source=Naijagracerace`
+        socialWindow(url);
+    });
+    $(".facebook").click(function(e) {
+      e.preventDefault();
+  
+      var this_ = $(this)
+      var path = this_.attr("data-url");
+      var pageUrl = `${origin}${path}`
+      url = `https://www.facebook.com/sharer.php?u=${pageUrl}?utm_source=Naijagracerace`
+      socialWindow(url);
+  });
+      $(".linkedin").click(function(e) {
+        e.preventDefault();
+  
+        var this_ = $(this)
+        var path = this_.attr("data-url");
+        var pageUrl = `${origin}${path}`
+        url =  `http://www.linkedin.com/shareArticle?mini=true&url=${pageUrl}?utm_source=Naijagracerace` 
+        socialWindow(url);
+    });
+    $(".whatsapp").click(function(e) {
+      e.preventDefault();
+  
+      var this_ = $(this)
+      var path = this_.attr("data-url");
+      var pageUrl = `${origin}${path}`
+      url =  `whatsapp://send?text=${pageUrl}?utm_source=Naijagracerace` 
+      socialWindow(url);
+  });
+  $(".copy-link").click(function(e) {
+    e.preventDefault();
+  
+    var this_ = $(this)
+    var path = this_.attr("data-url");
+    var pageUrl = `${origin}${path}?utm_source=Naijagracerace`
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(pageUrl).select();
+    document.execCommand("copy");
+    $temp.remove();
+    
+  });
+  
